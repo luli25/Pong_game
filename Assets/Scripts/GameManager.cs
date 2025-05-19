7c3a94f303b52fb3b4d78b4a39a6872084100772
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2025-05-19 13:36:16 -0300

    Created font asset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private TMP_Text paddle1ScoreText;
+
+    [SerializeField]
+    private TMP_Text paddle2ScoreText;
+
+    [SerializeField]
+    private Transform paddle1Transform;
+
+    [SerializeField]
+    private Transform paddle2Transform;
+
+    [SerializeField]
+    private Transform ballTransform;
+
+    private int paddle1Score;
+    private int paddle2Score;
+
+    private static GameManager instance;
+
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindAnyObjectByType<GameManager>();
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Paddle1Score()
     {
-        
+        paddle1Score++;
+        paddle1ScoreText.text = paddle1Score.ToString();
     }
+
+    public void Paddle2Score()
+    {
+        paddle2Score++;
+        paddle2ScoreText.text = paddle2Score.ToString();
+    }
+
+    public void Restart()
+    {
+        paddle1Transform.position = new Vector2(paddle1Transform.position.x, 0);
+        paddle2Transform.position = new Vector2(paddle2Transform.position.x, 0);
+        ballTransform.position = Vector2.zero;
+    }
+
 }

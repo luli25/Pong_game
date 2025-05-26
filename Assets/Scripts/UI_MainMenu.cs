6826edca660ac2644f28457cfe10acded59aca6a
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2025-05-26 13:58:16 -0300

    Finished Main Menu UI

--- Assets/Scripts/UI_MainMenu.cs
+++ Assets/Scripts/UI_MainMenu.cs
@@ -1,16 +1,41 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_MainMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+    [SerializeField]
+    private GameObject mainMenuPanel;
+
+    [SerializeField]
+    private GameObject maxScorePanel;
+
+    [SerializeField]
+    private Button newGameButton;
+
+    [SerializeField]
+    private Button exitButton;
+
+    private void Start()
     {
-        
+        // Initialize the main menu
+        mainMenuPanel.SetActive(true);
+        maxScorePanel.SetActive(false);
+        newGameButton.onClick.AddListener(OnNewGameButtonClicked);
+        exitButton.onClick.AddListener(ExitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnNewGameButtonClicked()
     {
-        
+        mainMenuPanel.SetActive(false);
+        maxScorePanel.SetActive(true);
+    }
+
+    private void ExitGame()
+    {
+        Debug.Log("Exiting game...");
+        Application.Quit();
     }
 }
